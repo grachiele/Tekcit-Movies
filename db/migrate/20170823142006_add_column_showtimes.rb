c4287a18d1e2dbf79ea95124653af4dc218a8a3d
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-11-05 01:15:09 -0400

    changing db format for heroku, removing default ticket count

--- db/migrate/20170823142006_add_column_showtimes.rb
+++ db/migrate/20170823142006_add_column_showtimes.rb
@@ -1,5 +1,5 @@
 class AddColumnShowtimes < ActiveRecord::Migration[5.1]
   def change
-    add_column :showtimes, :tickets_left, :integer, :default 10
+    add_column :showtimes, :tickets_left
   end
 end
